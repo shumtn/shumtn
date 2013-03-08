aeda71a13b526b67b119bedc7ac827997e11df16
daijunjie <djj@shumtn.com>
2013-03-08 21:20:00 +0800

    Revert "Revert "delete libuvext for shumtn""
    This reverts commit 43edf3fe95f82b25fc36853ccf7964ac70c51e08.

--- libuvext/src/include/netserver.h
+++
@@ -1,6 +0,0 @@
-#include "channel.h"
-
-UV_EXTERN void net_server_create(char* ip, int port, int backlog, int maxConnects, int bufferSize, void* connect_cb, void* write_cb, void* read_cb, void* close_cb, void* error_cb);
-UV_EXTERN void net_server_write(void* handle, char* data, ssize_t size);
-
-
